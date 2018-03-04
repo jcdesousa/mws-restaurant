264a732be7258c0db59ca3f310801b0862abe992
José Sousa <jcsousa9@gmail.com>
2018-03-04 13:41:13 +0000

    feat: Add alt attributes to images

--- js/main.js
+++ js/main.js
@@ -141,6 +141,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' Details Image';
+
   li.append(image);
 
   const name = document.createElement('h1');
