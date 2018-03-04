af4de555618dbaf731202b8f1f5b2070bf260273
José Sousa <jcsousa9@gmail.com>
2018-03-04 13:47:05 +0000

    feet: Add ARIA roles to elements

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', 'View Details for ' + restaurant.name);
   li.append(more)
 
   return li
