178dd339585077aafeed1cd4417cee184bec309f
José Sousa <jcsousa9@gmail.com>
2018-06-14 00:20:33 +0100

    Register service worker

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,11 +1,10 @@
 import 'normalize.css';
 import '../css/core.css';
 import GoogleMapsLoader from 'google-maps';
+import registerServiceWorker from './sw-register';
 
 GoogleMapsLoader.KEY = 'AIzaSyDiRsB2-0W-Xvg4-mntbBG3DHYusvpBeCc';
 
+registerServiceWorker();
+
 export { GoogleMapsLoader };
-
-// import registerServiceWorker from './sw-register';
-
-// registerServiceWorker();
